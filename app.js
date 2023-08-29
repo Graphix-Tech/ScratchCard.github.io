daf4ddc7a0e682022b2ddf868389796fc0efd07a
puja0709 <102405052+puja0709@users.noreply.github.com>
2023-08-29 15:04:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,68 +8,33 @@ $("#card").wScratchPad({
 });
 */
 
-/*const cardContainer = document.getElementById("card");
 
-// Create an array of image URLs
-const imageUrls = [
-  "Images/Gpay_Card1.jpg",
-  "Images/Gpay_Card2.jpg",
-  "Images/Gpay_Card3.jpg",
-  "Images/Gpay_Card4.jpg",
-];
+// Check if the scratch card has been revealed before
+let isRevealed = localStorage.getItem("isRevealed") === "true";
 
-let shuffledImages = shuffleArray(imageUrls);
-let currentImageIndex = 0;
-let scratched = false;
-
-// Function to shuffle an array randomly
-function shuffleArray(array) {
-  return array.sort(() => Math.random() - 0.5);
-}
-
-// Function to start scratch card
-function startScratchCard() {
-  if (!scratched) {
-    cardContainer.innerHTML = ""; // Clear previous content
-    const scratchPad = $("<div>")
-      .addClass("scratch-pad")
-      .wScratchPad({
-        size: 30,
-        bg: shuffledImages[currentImageIndex],
-        fg: "Images/front.jpg",
-        scratchUp: function () {
-          scratched = true;
-        },
-      });
-
-    $(cardContainer).append(scratchPad);
-  }
-}
-
-// Initialize the scratch card
-startScratchCard();
-*/
-
-// This code should come before the scratch card initialization
-let isRevealed = false; // Flag to track whether the scratch card has been revealed
-
-// The following code initializes the scratch card with the shuffling images and reveal logic
 $(document).ready(function () {
-  const numImages = 4; // Number of available images
-  const randomImageIndex = Math.floor(Math.random() * numImages) + 1;
+  if (!isRevealed) {
+    // The following code initializes the scratch card with the shuffling images and reveal logic
+    const numImages = 4; // Number of available images
+    const randomImageIndex = Math.floor(Math.random() * numImages) + 1;
 
-  $("#card").wScratchPad({
-    size: 100, // The size of the brush/scratch.
-    bg: `Images/Gpay_Card ${randomImageIndex}.jpg`, // Background image path.
-    fg: `Images/front.jpg`, // Foreground image path.
-    cursor: "pointer", // Set cursor.
-    scratchUp: function () {
-      if (!isRevealed) {
-        isRevealed = true;
-        // Add any action you want to perform when the scratch card is revealed
-        alert("Scratch card revealed!");
-      }
-    },
-  });
+    $("#card").wScratchPad({
+      size: 100, // The size of the brush/scratch.
+      bg: `Images/Gpay_Card ${randomImageIndex}.jpg`, // Background image path.
+      fg: `Images/front.jpg`, // Foreground image path.
+      cursor: "pointer", // Set cursor.
+      scratchUp: function () {
+        if (!isRevealed) {
+          isRevealed = true;
+          // Save the revealed state in local storage
+          localStorage.setItem("isRevealed", "true");
+          // Add any action you want to perform when the scratch card is revealed
+          alert("Scratch card revealed!");
+        }
+      },
+    });
+  } else {
+    // If the card has been revealed, you can show a message or perform other actions
+    alert("Scratch card already revealed!");
+  }
 });
-
